9c908297ab1f0d0d2b6bf2a13fff58e5d8336214
Anikesh <anikesh@0725@gmail.com>
2021-04-26 19:48:16 +0530

    Addition : Uc2 Regex for Last Name

--- userRegestration/RegexSolver.java
+++ userRegestration/RegexSolver.java
@@ -5,7 +5,8 @@ import java.util.regex.Pattern;
 
 public class RegexSolver  {	
   String validFirstNameFormat = "^[A-Z][a-z]{2,}";
-	boolean validate(String userInput)
+  String validLastNameFormat = "^[A-Z][a-z]{2,}";
+	void validate(String userInput )
 	 {
 		
 		Pattern compile =Pattern.compile( validFirstNameFormat  ); 
@@ -15,16 +16,41 @@ public class RegexSolver  {
 		Boolean result = match.matches();
 		if(result)
 		{
-			System.out.println(" Valid \n First name Submitted");
+			System.out.println("Valid \n First name  Submitted");
 			
-		 return result;
+		 
 		}else 
 			System.out.println("Not valid\n Please check the Validation \n"
 					+ "\n  First Alphabet Should Be In Capital \n"
 					+ "  Length should be atleast 3 ");
-			return false;
+			
 		
 		
 		
-	 }
+	    }
+
+
+
+void validatelastName(String userInput )
+{
+	
+	Pattern compile =Pattern.compile( validLastNameFormat  ); 
+	
+	Matcher match= compile.matcher(userInput);
+	
+	Boolean result = match.matches();
+	if(result)
+	{
+		System.out.println("Valid \n Last name Submitted");
+		
+	 
+	}else 
+		System.out.println("Not valid\n Please check the Validation \n"
+				+ "\n  First Alphabet Should Be In Capital \n"
+				+ "  Length should be atleast 3 ");
+		
+	
+	
+	
+   }
 }
--- userRegestration/UserRegestrationProblem.java
+++ userRegestration/UserRegestrationProblem.java
@@ -6,13 +6,19 @@ import java.util.Scanner;
 public class UserRegestrationProblem {
 	
 	public static void main(String[] args) {
-			System.out.println("Enter the First name");
-		
-			Scanner scanner = new Scanner(System.in);
-			String userInput = scanner.nextLine();
+		    Scanner scanner = new Scanner(System.in);
+		    RegexSolver regexSolver = new  RegexSolver();
+			
+		    System.out.println("Enter the First name");
+			String userFirstName = scanner.next();
+			
+			regexSolver.validate(userFirstName);
+			System.out.println("\n<-------->\n");
+			
+		    System.out.println("Enter the last name");
+			String userLastName = scanner.next();
 		 
-		RegexSolver regexSolver = new  RegexSolver();
-			regexSolver.validate(userInput);
+			regexSolver.validatelastName(userLastName);
 		scanner.close();
 	}
 }
