5b6ce7f3c33c895072fb53f853e1fe35b9a6b49e
anikeshmahajan <36470775+anikeshmahajan@users.noreply.github.com>
2021-04-27 12:02:29 +0530

    Update UserRegestrationProblem.java

--- userRegestration/UserRegestrationProblem.java
+++ userRegestration/UserRegestrationProblem.java
@@ -8,36 +8,36 @@ public class UserRegestrationProblem {
 	public static void main(String[] args) {
 		    Scanner scanner = new Scanner(System.in);
 		    RegexSolver regexSolver = new  RegexSolver();
-//				
-//			    System.out.println("Enter the First name");
-//				String userFirstName = scanner.nextLine();
-//				
-//				regexSolver.validate(userFirstName);
-//				System.out.println("\n<-------->\n");
-//				
-//				scanner.nextLine();
-//		    System.out.println("Enter the last name");
-//			String userLastName = scanner.nextLine();
-//		 
-//			regexSolver.validatelastName(userLastName);
-//			
-//            System.out.println("\n<-------->\n");
-//			   
-//            scanner.nextLine();
-//			    System.out.println("Enter the email");
-//				String emailId = scanner.nextLine();
-//				
-//				regexSolver.checkEmailId(emailId);
-//				
-//				 System.out.println("\n<-------->\n");
-//					
-//			System.out.println("Enter the MobileNo");
-//			String mobileNo = scanner.nextLine();
-//				
-//			regexSolver.checkMobileNo(mobileNo);
-//			
-//			System.out.println("\n<-------->\n");
-//			
+				
+			    System.out.println("Enter the First name");
+				String userFirstName = scanner.nextLine();
+				
+				regexSolver.validate(userFirstName);
+				System.out.println("\n<-------->\n");
+				
+				scanner.nextLine();
+		    System.out.println("Enter the last name");
+			String userLastName = scanner.nextLine();
+		 
+			regexSolver.validatelastName(userLastName);
+			
+           System.out.println("\n<-------->\n");
+			   
+           scanner.nextLine();
+			    System.out.println("Enter the email");
+				String emailId = scanner.nextLine();
+				
+				regexSolver.checkEmailId(emailId);
+				
+				 System.out.println("\n<-------->\n");
+					
+			System.out.println("Enter the MobileNo");
+			String mobileNo = scanner.nextLine();
+				
+			regexSolver.checkMobileNo(mobileNo);
+			
+			System.out.println("\n<-------->\n");
+			
 			System.out.println("Enter Your Password");
 			String password = scanner.nextLine();
 			regexSolver.passwordValid(password);
